7704149b6a90d78e9228d4d474f90ccd2d393f75
Paulino Salvador Omnitec <paulino.salvador@omnitecsystems.com>
2026-04-05 00:45:42 +0200

    refactor: Renombrar index.js a main.jsx para compatibilidad con Vite

---
+++ src/Projects/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './react-task-app/src/App.jsx';
+import './react-task-app/src/index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
